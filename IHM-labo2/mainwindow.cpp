8f706d09e345343356356d91b118022b5a8923c3
SashaKor <alexandra.korukova@gmail.com>
2018-11-04 12:24:57 +0100

    files to ignore removal

--- IHM-labo2/mainwindow.cpp
+++ IHM-labo2/mainwindow.cpp
@@ -6,11 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    // set the icon for the Open File buton
-    QPixmap openButtonPixmap("qrc:/imgs/baseline-folder_open-24px.svg");
-    QIcon openButtonIcon(openButtonPixmap);
-    ui->pushButton->setIcon(openButtonIcon);
-    ui->pushButton->setIconSize(openButtonPixmap.rect().size());
+    ui->outputGroupBox->setChecked(false);
 }
 
 MainWindow::~MainWindow()
